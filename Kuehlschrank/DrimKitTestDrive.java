9ff3b2d8b09bd5df4d59d7506d322f22131facca
Mamonov Victor <mamonov.victor@gmail.com>
2020-03-19 21:55:59 +0100

    DrimKit

---
+++ Kuehlschrank/DrimKitTestDrive.java
@@ -0,0 +1,12 @@
+class DrimKitTestDrive {
+	public static void main (String[] args) {
+		DrimKit d = new DrimKit();
+		d.snare = true;
+		
+		if (d.snare = true) {
+			d.playSnare();
+		}
+		d.snare = false;
+		d.playTopHat();
+	}
+}
